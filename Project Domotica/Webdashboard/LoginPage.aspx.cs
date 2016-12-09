e5e1786d9ea2769f3c6126dcd281e203c73ac6a2
joris <joris@JORIS-PC>
2016-12-09 14:19:00 +0100

    LoginPageUpdate1.2

--- Project Domotica/Webdashboard/LoginPage.aspx.cs
+++ Project Domotica/Webdashboard/LoginPage.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Configuration;
+
 
 namespace Webdashboard
 {
@@ -11,9 +14,30 @@ namespace Webdashboard
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
 
+        
         }
 
-       
+        protected void btnRegistreren_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection();
+            conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; " +
+                "Data Source=|DataDirectory|LoginData.accdb";
+
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "INSERT INTO GebruikersTabel (GebruikersNaam,Wachtwoord,Voornaam,Achternaam,Geboortedatum,Email)";
+
+            try
+            {
+                conn.Open();
+                OleDbDataReader reader = cmd.ExecuteReader();
+                reader.Close();
+            }
+            catch (Exception exc) { lblConnectionFeedback.Text = exc.Message; }
+            finally { conn.Close(); }
+
+        }
     }
 }
