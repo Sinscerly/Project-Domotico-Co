a08c628be328f3bf52cbe4191825e91019da5b05
Thomas Home <Thomas Home@DESKTOP-JUGBUJI>
2017-01-29 22:00:27 +0100

    Commit

--- Project Domotica/Webdashboard/DashboardMaster.Master.cs
+++ Project Domotica/Webdashboard/DashboardMaster.Master.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.OleDb;
 
 namespace Webdashboard
 {
@@ -11,7 +12,55 @@ namespace Webdashboard
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string ID = string.Empty, USER = string.Empty;
+            HttpCookie CookieLogin = Request.Cookies["CookieID"];
+            if (CookieLogin != null)
+            {
+                ID = CookieLogin["UserID"].ToString();
+            }
+            if (ID != null)
+            {
+                OleDbConnection conn = new OleDbConnection();
+                conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" +
+                    Server.MapPath(@"\App_data") + @"\DashboardDatabase.accdb";
 
+                OleDbCommand cmd = new OleDbCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = string.Format("SELECT username FROM users WHERE id = {0} ", ID);
+                try
+                {
+                    conn.Open();
+
+                    OleDbDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        USER = string.Format(reader["username"].ToString());
+                    }
+                }
+                catch (Exception exc) { lbl_WelcomeUser.Text = exc.Message; }
+                finally { conn.Close(); }
+
+                if (DateTime.Now.Hour < 12)
+                {
+                    lbl_WelcomeUser.Text = "Goedemorgen " + USER;
+                }
+                else if (DateTime.Now.Hour >= 12 && DateTime.Now.Hour < 18)
+                {
+                    lbl_WelcomeUser.Text = "Goedemiddag " + USER;
+                }
+                else if (DateTime.Now.Hour >= 18)
+                {
+                    lbl_WelcomeUser.Text = "Goedenavond " + USER;
+                }
+            }
+            else
+            {
+                lbl_WelcomeUser.Text = "U bent nog niet ingelogd.";
+            }
+        }
+        protected void Page_LoadComplete()
+        {
+            
         }
     }
 }
