ffacdd502389e49edf8ab97c0ce1f93ed291a898
Thomas Home <Thomas Home@DESKTOP-JUGBUJI>
2017-02-05 22:18:49 +0100

    login fix

--- Project Domotica/Webdashboard/DashboardMaster.Master.designer.cs
+++ Project Domotica/Webdashboard/DashboardMaster.Master.designer.cs
@@ -31,15 +31,6 @@ namespace Webdashboard {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// BtnLogout control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button BtnLogout;
-        
-        /// <summary>
         /// lbl_WelcomeUser control.
         /// </summary>
         /// <remarks>
@@ -47,6 +38,15 @@ namespace Webdashboard {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lbl_WelcomeUser;
+        
+        /// <summary>
+        /// BtnLogout control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnLogout;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
