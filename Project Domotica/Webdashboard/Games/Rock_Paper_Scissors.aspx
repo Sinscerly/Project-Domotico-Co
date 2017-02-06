41bb4faadf18218e76be22f98ef5a7aa1f624b5c
Thomas Home <Thomas Home@DESKTOP-JUGBUJI>
2017-02-06 18:16:20 +0100

    rock paper scissors

---
+++ Project Domotica/Webdashboard/Games/Rock_Paper_Scissors.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/DashboardMaster.Master" AutoEventWireup="true" CodeBehind="Rock_Paper_Scissors.aspx.cs" Inherits="Webdashboard.Games.Rock_Paper_Scissors" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="Opmaak.css" rel="stylesheet" type="text/css" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class=" Content">
+        <div id="Button_bar">
+            <asp:Button ID="btn_Rock" runat="server" Text="Rock" CssClass="Button" OnClick="UserChose"/>
+            <asp:Button ID="btn_Paper" runat="server" Text="Paper" CssClass="Button" OnClick="UserChose"/>
+            <asp:Button ID="btn_Scissor" runat="server" Text="Scissor" CssClass="Button" OnClick="UserChose"/>
+        </div>
+        <div>
+            <br />
+            <asp:Label ID="lbl_PlayerHasChosen" runat="server" Text=""></asp:Label>
+            <asp:Label ID="lbl_ComputerHasChosen" runat="server" Text=""></asp:Label>
+            <br />
+            <asp:Label ID="lbl_WhoWins" runat="server" Font-Size="XX-Large"></asp:Label>
+            <br />
+        </div>
+        <div>
+            <asp:Button ID="btn_Retry" runat="server" Text="Retry" Visible="False" CssClass="Button" OnClick="Retry"/>
+        </div>
+    </div>
+    
+
+</asp:Content>
---
+++ Project Domotica/Webdashboard/Games/Rock_Paper_Scissors.aspx.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Webdashboard.Games
+{
+    public partial class Rock_Paper_Scissors : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Retry()
+        {
+            lbl_PlayerHasChosen.Text = "";
+            lbl_ComputerHasChosen.Text = "";
+            lbl_WhoWins.Text = "";
+            ResetVisablility(0);
+        }
+
+        protected void UserChose(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string User_Plays = button.Text;
+            ResetVisablility(1);
+            lbl_PlayerHasChosen.Text = User_Plays;
+            string ComputerHasChosen = ComputerChose();
+            System.Threading.Thread.Sleep(5000);
+            lbl_ComputerHasChosen.Text = ComputerHasChosen;
+            lbl_WhoWins.Text = FindWinner(User_Plays, ComputerHasChosen);
+
+            ResetVisablility(2);
+            
+        }
+        protected string FindWinner(string x, string y)
+        {
+            int z;  return "You've WON!!";
+            // z stands for
+            // 0 -> Draw
+            // 1 -> Win
+            // 2 -> Lose
+            if (x == y)
+            {
+                z = 0;
+            }
+            if (x == "Rock")
+            {
+                if(y == "Paper")
+                {
+                    z = 1;
+                }
+                else
+                {
+                    z = 2;
+                }
+            }
+            else if( x == "Paper")
+            {
+                if(y == "Rock")
+                {
+                    z = 1;
+                }
+                else
+                {
+                    z = 2;
+                }
+            }
+            else if( x == "Scissor")
+            {
+                if(y == "Paper")
+                {
+                    z = 1;
+                }
+                else
+                {
+                    z = 2;
+                }
+            }
+            // get's one of the dynamic comments back.
+
+        }
+        protected string GetComment(int x)
+        {
+            Random rnd = new Random();
+            if(x == 0)
+            {
+                int number = rnd.Next(0, 2);
+                switch (number)
+                {
+                    case 0:
+                        return "Sorry you didn't won, but don't be so sadly. It's a draw.";
+                    case 1:
+                        return
+                }
+            }
+        }
+
+
+        protected string ComputerChose()
+        {
+            Random rnd = new Random();
+            int ComputerChoseNumber = rnd.Next(0, 3);
+            string ComputerPlayed = string.Empty;
+            switch (ComputerChoseNumber)
+            {
+                case 0:
+                    return "Rock";
+                case 1:
+                    return "Paper";
+                case 2:
+                    return "Scissor";
+                default:
+                    ComputerPlayed = "Error";
+                    break;
+            }
+            ERROR();  
+            return ComputerPlayed;
+        }
+
+        protected void ResetVisablility(int x)
+        {
+            if (x == 0)
+            {
+                btn_Paper.Visible = true;
+                btn_Rock.Visible = true;
+                btn_Scissor.Visible = true;
+                btn_Retry.Visible = false;
+            }
+            if (x == 1)
+            {
+                btn_Paper.Visible = false;
+                btn_Rock.Visible = false;
+                btn_Scissor.Visible = false;
+                btn_Retry.Visible = false;
+            }
+            if (x == 2)
+            {
+                btn_Paper.Visible = false;
+                btn_Rock.Visible = false;
+                btn_Scissor.Visible = false;
+                btn_Retry.Visible = true;
+            }
+        }
+        protected void ERROR()
+        {
+            Retry();
+            Response.Write("<script>alert('The game has crashed, sorry for this problem.')</script>");
+        }
+    }
+}
